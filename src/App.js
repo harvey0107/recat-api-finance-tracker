0f1b3f71c28789ae40d3e0878b75c0f11d4dc957
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-06-26 15:18:09 -0400

    adde coin componemt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState }   from 'react';
 import './App.css';
+import axios from 'axios'
 
 const apiUrl = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
 
 function App() {
+  const [coins , setCoins] = useState([])
+  useEffect(()=>{
+    axios.get(apiUrl)
+    .then(res => {
+      setCoins(res.data)
+      console.log(res.data)
+    })
+    .catch(console.error) 
+  },[]);
   return (
     <div className="App">
-      <h1>React API Tracker</h1>
+      <div classNamw="search">
+        <h1 classNamw="text">Search Currency</h1>
+        <form>
+          <input type="text" className="input" placeholder="Search" />
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/Coin.js
@@ -0,0 +1,12 @@
+import { ReactComponent } from "*.svg";
+import React from 'react'
+
+const Coin = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Coin
